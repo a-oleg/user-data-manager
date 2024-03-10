5ee36c36ae423051aab7a82b7766dfa3454f4361
Oleg <oleg.arhipovski@gmail.com>
2024-03-10 16:56:10 +0300

    Added the main method

---
+++ src/main/java/userdatamanager/aoleg/com/github/UserDataManagerApplication.java
@@ -0,0 +1,11 @@
+package userdatamanager.aoleg.com.github;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataManagerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserDataManagerApplication.class, args);
+    }
+}
