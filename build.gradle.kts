24eff1f9cc580f08e940a5a906c12c15ad1b8808
andreysvyat <andreysvyatoy@gmail.com>
2024-03-18 23:10:54 +0300

    add swagger

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
 plugins {
     id("java")
+    id("org.openapi.generator") version "7.4.0"
 }
 
 group = "aoleg.com.github"
@@ -11,6 +12,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-web:3.2.3")
+    implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.3.0")
     compileOnly("org.projectlombok:lombok:1.18.30")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
@@ -18,4 +20,29 @@ dependencies {
 
 tasks.test {
     useJUnitPlatform()
+}
+
+openApiGenerate {
+    inputSpecRootDirectory = "$projectDir/swagger"
+    generatorName = "spring"
+    library = "spring-http-interface"
+    outputDir = "$projectDir"
+    modelPackage = "userdatamanager.aoleg.com.github.dto"
+    apiPackage = "userdatamanager.aoleg.com.github.api"
+    generateModelDocumentation = true
+    generateApiDocumentation = true
+    generateApiTests = false
+    generateModelTests = false
+    skipOperationExample = true
+    configOptions.set(hashMapOf(
+            "generateForOpenFeign" to "true",
+            "configPackage" to "userdatamanager.aoleg.com.github.config",
+            "documentationProvider" to "true",
+            "openApiNullable" to "false",
+            "useResponseEntity" to "false"
+    ))
+}
+
+openApiValidate {
+    inputSpec.set("$projectDir/swagger/openapi.yaml")
 }
